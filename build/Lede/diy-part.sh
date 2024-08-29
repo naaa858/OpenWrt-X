9aed87c1608b8f5fabb7c000321c8890c064f349
Namia-A <mk258855@outlook.com>
2024-08-29 17:13:10 +0800

    Update diy-part.sh

--- build/Lede/diy-part.sh
+++ build/Lede/diy-part.sh
@@ -78,8 +78,6 @@ export Delete_unnecessary_items="0"          # 个别机型内一堆其他机型
 export Disable_53_redirection="0"            # 删除DNS强制重定向53端口防火墙规则(个别源码本身不带此功能)(1为启用命令,填0为不作修改)
 export Cancel_running="0"                    # 取消路由器每天跑分任务(个别源码本身不带此功能)(1为启用命令,填0为不作修改)
 
-git clone https://github.com/morytyann/OpenWrt-mihomo;main
-
 # 16----------------------------------------------------------------------------------------------------------------------------------
 # 晶晨CPU系列打包固件设置(不懂请看说明)
 export amlogic_model="s905d"
